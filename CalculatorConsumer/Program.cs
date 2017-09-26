3eeddef565b087ab9c19c771b666546f3d3ca758
casperv <casp3559@edu.easj.dk>
2017-09-26 10:41:53 +0200

    WCF calc

---
+++ CalculatorConsumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.CalculatorService1Client calc = new ServiceReference1.CalculatorService1Client();
+
+            Console.WriteLine(calc.Add(3, 3));
+        }
+    }
+}
--- WcfService1/ICalculatorService1.cs
+++ WcfService1/ICalculatorService1.cs
@@ -8,10 +8,12 @@ using System.Text;
 
 namespace WcfService1
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculatorService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface ICalculatorService1
     {
+        [OperationContract]
+        double Add(double a, double b);
 
         [OperationContract]
         string GetData(int value);
